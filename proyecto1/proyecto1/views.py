b21d96d044a76cb3758caf11ae1b297c2b59b6a9
Ezequielleston <ezequielleston9@gmail.com>
2023-08-22 14:21:50 -0300

    fin primer proyecto1.0

--- proyecto1/proyecto1/views.py
+++ proyecto1/proyecto1/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.template import Template, Context
+from django.template import Template, Context, loader
 
 
 def saludar(request):
@@ -11,14 +11,19 @@ def segundaVista(request):
 def saludo_con_nombre(request, nombre):
     return HttpResponse(f"<h1>Hola {nombre}</h1>")
 
-def probandoHtml(requets):
+#def probandoHtml(requets):
+    #diccionario={"nombre":"Eze", "apellido":"Leston", "edad":19, "lista_de_notas":[1,5,6,7,8,9,2]}
+    #archivo=open(r"C:\Users\ezequ\OneDrive\Escritorio\pythonproyecto1.0\proyecto1\plantillas\template1.html")
+    #archivo.close()
+    #template=Template(contenido)
+    #contexto=Context(diccionario)
+    #documento=template.render(contexto)
+    #return HttpResponse(documento)
 
-    diccionario={"nombre":"Eze", "apellido":"Leston", "edad":19}
 
-    archivo=open(r"C:\Users\ezequ\OneDrive\Escritorio\pythonproyecto1.0\proyecto1\plantillas\template1.html")
-    contenido=archivo.read()
-    archivo.close()
-    template=Template(contenido)
-    contexto=Context(diccionario)
-    documento=template.render(contexto)
+def probandoHtml(request):
+
+    diccionario={"nombre":"Eze", "apellido":"Leston", "edad":19, "lista_de_notas":[1,5,6,7,8,9,2]}
+    template=loader.get_template("template1.html")   
+    documento=template.render(diccionario)
     return HttpResponse(documento)
